30514255c74a90e7690b8d84c3566c10c1015c11
ek <kalininei@yandex.ru>
2025-09-08 22:45:34 +0300

    vs-code settings revised

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,10 @@ RUN apt-get install -y python3 python3-pip
 RUN apt-get install -y libfmt-dev libtbb-dev catch2
 RUN apt-get install -y mc vim curl wget sudo 
 RUN apt-get install -y clang-format clang-tidy clangd
+RUN apt-get install -y iputils-ping
+
+RUN pip3 install --break-system-packages --no-cache-dir black
+RUN pip3 install --break-system-packages --no-cache-dir cmake-format
 
 RUN update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-14 100 && \
     update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-14 100 && \
@@ -24,5 +28,7 @@ USER user
 RUN echo 'export CC=/usr/bin/gcc-14' >> /home/user/.bashrc
 RUN echo 'export CXX=/usr/bin/g++-14' >> /home/user/.bashrc 
 
-#WORKDIR /app
+RUN mkdir -p /home/user/.vscode-server
+
+WORKDIR /app
 CMD tail -f /dev/null
--- formatall.sh
+++ formatall.sh
@@ -1,1 +1,47 @@
-find src/ -type f \( -name "*.h" -o -name "*.cpp" -o -name "*.c" -o -name "*.hpp" -o -name "*.cc" \) -print0 | xargs -0 clang-format -i -style=file
+#!/bin/bash
+set -e
+
+exclude_paths=(-not -path "./.*" -not -path "./build*" -not -path "*/venv/*" -not -path "*/env/*" -not -path "*/__pycache__/*")
+
+echo "=== START FORMATTING ==="
+
+format_file() {
+    local formatter="$1"
+    local file="$2"
+    local extension="${file##*.}"
+    
+    local temp_file=$(mktemp)
+    cp "$file" "$temp_file"
+    
+    $formatter "$temp_file" >/dev/null 2>&1
+    
+    if ! diff -q "$file" "$temp_file" >/dev/null; then
+        echo " format: $file"
+        mv "$temp_file" "$file"
+    else
+        rm -f "$temp_file"
+    fi
+}
+
+# 1. C++ файлы
+echo "CHECK C++ ..."
+find . \( -name "*.cpp" -o -name "*.hpp" -o -name "*.h" -o -name "*.cc" -o -name "*.cxx" \) "${exclude_paths[@]}" | \
+while read file; do
+    format_file "clang-format -i -style=file" "$file"
+done
+
+# 2. Python файлы  
+echo "CHECK PYTHON..."
+find . \( -name "*.py" \) "${exclude_paths[@]}" | \
+while read file; do
+    format_file "black -q" "$file"
+done
+
+# 3. CMake файлы
+echo "CHECK CMake..."
+find . \( -name "CMakeLists.txt" -o -name "*.cmake" \) "${exclude_paths[@]}" | \
+while read file; do
+    format_file "cmake-format -i" "$file"
+done
+
+echo "=== DONE ==="
